88f4651553238acf32e1a2e6197d237052b16242
Moritz Miedler <miedler.m03@htlwienwest.at>
2022-01-26 14:28:58 +0100

    x

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -27,6 +27,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #2c3e50;
 }
 </style>
